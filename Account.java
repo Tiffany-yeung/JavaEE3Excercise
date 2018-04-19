e770840b8408fc8f6ca4f78fd79480b174ad577d
Tiffany-yeung <37367598+Tiffany-yeung@users.noreply.github.com>
2018-04-19 02:11:16 +0100

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,10 +1,10 @@
-package Maven2.Excercises;
+package com.qa.domain;
 
 public class Account {
     	
 	private String firstName;
 	private String lastName;
-	private String accountNo;
+	private int accountNo;
 	
 	public String getFirstName() {
 		return firstName;
@@ -14,12 +14,12 @@ public class Account {
 		return lastName;
 	}
 
-	public String getAccountNo() {
+	public int getAccountNo() {
 		return accountNo;
 	}
 
-	public Account (String firstName, String lastName, String accountNo) {
-		this.firstName = firstName;	//setters
+	public Account (String firstName, String lastName, int accountNo) {
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.accountNo = accountNo;
 	}
